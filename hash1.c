587cd75ea45aba48a083d264eceda79963eec075
Anargyros Argyros <argyrosan@gmail.com>
2022-10-17 13:32:04 +0300

    ordering of relations

--- hash1.c
+++ hash1.c
@@ -23,8 +23,15 @@ void init_histogram(int* histogram,int histogram_size){
 }
 
 
+
+void init_relation(relation* rel,int num_tuples){
+    rel->tuples = malloc(num_tuples * sizeof(tuple));
+    rel->num_tuples = num_tuples;
+}
+
+
 //partition the original relation in 2^n buckets 
-relation* partition_relation(relation relation,int n){
+relation* partition_relation(relation rel,int n){
     //Our histograms are int arrays where the index is the bucket hash value and the number pointed to
     //by the index is the number of occurences
     int histogram_size = 2<<(n-1);
@@ -33,8 +40,8 @@ relation* partition_relation(relation relation,int n){
     init_histogram(histogram,histogram_size);
 
     //fill histogram by hashing the payloads of the relation tuples and counting the results
-    for(int i=0;i<relation.num_tuples;i++){
-        histogram[hash1(relation.tuples[i].payload,n)]++;
+    for(int i=0;i<rel.num_tuples;i++){
+        histogram[hash1(rel.tuples[i].payload,n)]++;
     }   
 
     //debug line
@@ -47,5 +54,20 @@ relation* partition_relation(relation relation,int n){
         prefix_sum[i]=sum;
         sum += histogram[i];       
     }
+    relation ordered_rel;
+    init_relation(&ordered_rel,rel.num_tuples);
+    printf("rel inited\n");
+    //a table to keep the last index of each bucket that an element was inserted in
+    int offsets[histogram_size];
+    init_histogram(offsets,histogram_size);
+    for(int i=0;i<rel.num_tuples;i++){
+        int bucket = hash1(rel.tuples[i].payload,n);
+        ordered_rel.tuples[prefix_sum[bucket] + offsets[bucket]]= rel.tuples[i];
+        offsets[bucket]++;
+    }
     
+    for(int i=0;i<ordered_rel.num_tuples;i++){
+        printf("Element with payload %d and hashed value %d\n",ordered_rel.tuples[i].payload,hash1(ordered_rel.tuples[i].payload,n));
+    }
+
 }
