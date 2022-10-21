6f90a55dd403cbacd12c973f34d06272428623ce
Gianaras <Giannismav120@gmail.com>
2022-10-21 22:30:44 +0300

    hash table first draft

--- hashtable.c
+++ hashtable.c
@@ -1,61 +1,180 @@
 #include "hashtable.h"
 
-// creates a bucket, n is the size of the bitmap
-hashbucket inithashbucket(int n){
-    hashbucket bucket;
-    bucket.rowid = -1;
-    bucket.bitmap = malloc(n*sizeof(int));
-    int i = 0;
-    for(i=0; i<n;i++){
-        bucket.bitmap[i]=0;
-    }
-    return bucket;
-    
+// NOTES:
+// - on rehash, neighborhood size does not increase. May change
+// - hash function used for hash table seems kinda bad, but the rehashes might be because of the small, static neighborhood
+// - hash table implementation doesn't use actual bitmap yet, so it's probably slightly less efficient
+// - hopscotch has not been tested extremely thoroughly, but seems to work fine
+
+// hash function for hash table. Max limits output range
+unsigned int hash2(unsigned int x, unsigned int max) {
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = (x >> 16) ^ x;
+    return x % max;
 }
 
-hashtable *inithashtable(int n, int H){
+
+// returns 0 or 1 depending on whether bitmap is full (aka when everything is 1)
+int bitmap_full(int* bitmap, int size) {
+    for (int i=0; i<size; i++)
+        if (bitmap[i] == 0) return 0;
+    return 1;
+}
+
+
+// prints hash table, for debug purposes
+void print_hashtable(hashtable* table) {
+    printf("\n---------------------------\n");
+    for (int i=0; i<table->tablesize; i++) {
+        printf("%d: %d, ", i, table->htbuckets[i].rowid);
+        for (int j=0; j<table->nbsize; j++) {
+            printf("%d", table->htbuckets[i].bitmap[j]);
+        }
+        printf("\n");
+    }
+}
+
+
+// creates a bucket, n is the size of the bitmap
+hashbucket init_hashbucket(int n) {
+    hashbucket bucket;
+    bucket.rowid = -1;
+    bucket.key = 0;
+    bucket.bitmap = malloc(n*sizeof(int));
+
+    int i = 0;
+    for(i=0; i<n;i++)
+        bucket.bitmap[i]=0;
+    return bucket;   
+}
+
+
+// swaps payload of buckets and updates bitmap (assumes first bucket is empty, so this should only be used for hopscotch)
+// "bit" shows which bit of the bitmap to nullify
+void bucket_swap(hashtable* table, int empty_bucket, int other_bucket, int bit) {
+    table->htbuckets[empty_bucket].rowid = table->htbuckets[other_bucket+bit].rowid;
+    table->htbuckets[empty_bucket].key = table->htbuckets[other_bucket+bit].key;
+    table->htbuckets[other_bucket+bit].rowid = -1;
+    table->htbuckets[other_bucket+bit].key = 0;
+    table->htbuckets[other_bucket].bitmap[bit] = 0;
+    table->htbuckets[other_bucket].bitmap[empty_bucket-other_bucket] = 1;
+}
+
+
+// initialize hash table
+hashtable *init_hashtable(int n, int H) {
     hashtable *ht;
     ht = malloc(sizeof(hashtable));
-    //size of hashtable starts as 2*n, it may expand later...
+
+    //size of hashtable starts as 2*n, it may expand later
     ht->tablesize = 2*n;
     ht->nbsize = H;
     ht->htbuckets = malloc((2*n)*sizeof(hashbucket));
-    int i=0;
-    for(i=0; i<ht->tablesize; i++){
-        ht->htbuckets[i] = inithashbucket(H);
-    }
 
+    for (int i=0; i<ht->tablesize; i++)
+        ht->htbuckets[i] = init_hashbucket(H);
     return ht;
 }
 
-void expandhashtable(hashtable **ht){
-    //the new size of the hashtable will be double the old size and the size of the neighbourhood will be expanded as well
-    int new_size = (*ht)->tablesize *2;
-    int new_nbsize = (*ht)->nbsize *2;
 
-    hashtable *ht2 = inithashtable(new_size,new_nbsize);
+// takes key, and searches hash table. If search succeeds, the data (rowid) is stored in "ret"
+void search_hashtable(hashtable* table, int key, int* ret) {
+    int table_size = table->tablesize;
+    unsigned int hash_value = hash2(key, table_size);
 
-    //transfer all the data as it was before 
-    for(int i=0;i<new_size;i++){
-        if(i< (*ht)->tablesize){
-            ht2->htbuckets[i].rowid = (*ht)->htbuckets[i].rowid;
-            int j;
-            for(j=0; j< (*ht)->nbsize; j++){
-                ht2->htbuckets[i].bitmap[j] = (*ht)->htbuckets[i].bitmap[j];
-            }
+    for (int i=0; i<table->nbsize; i++) { // search neighborhood
+        if (table->htbuckets[hash_value+i].key == key) {
+            //printf("found rowid %d at index %d\n", table->htbuckets[hash_value+i].rowid, hash_value);
+            *ret = table->htbuckets[hash_value+i].rowid;
+            return;
         }
     }
-
-    free(*ht);
-    *ht = ht2;
-    return;
+    printf("could not find any data from key %d\n", key);
 }
 
-void deletehashtable(hashtable *ht){
-    int i=0;
-    for(i=0; i< ht->tablesize; i++){
-       free(ht->htbuckets[i].bitmap);
+
+// hashes key to insert data to hash table using hopscotch hashing
+hashtable* insert_hashtable(hashtable* table, int key, int data) {
+    int hashtable_full = 1, swapped, bit_num, pos;
+    int table_size = table->tablesize;
+    int neighborhood_size = table->nbsize;
+    unsigned int hash_value = hash2(key, table_size);
+
+    printf("\ninserting %d using key %d, mapping to index %u\n", data, key, hash_value);
+
+    // if bitmap for desired position is full, rehash
+    if (bitmap_full(table->htbuckets[hash_value].bitmap, neighborhood_size)) {
+        rehash_hashtable(&table, key, data);
+        return table;
     }
+
+    // linear search for empty space 
+    for (int i=hash_value; i<table_size; i++) {
+        if (table->htbuckets[i].rowid == -1) { // if we found an empty position
+            pos = i;
+            hashtable_full = 0;
+
+            while ((pos-(hash_value % table_size)) >= neighborhood_size) { // loop until we find position within neighborhood
+                bit_num = neighborhood_size-1;
+                swapped = 0;
+
+                for (int j=pos-neighborhood_size+1; j<pos; j++) { // for the previous H-1 positions,
+                    if (swapped) break;
+                    for (int k=0; k<bit_num; k++) { // for every bitmap bit that matters (as we move through positions, less bits matter)
+                        //printf("empty at %d, checking bucket %d, bit %d\n", pos, j, k);
+                        if (table->htbuckets[j].bitmap[k]) {
+                            bucket_swap(table, pos, j, k);
+                            //printf("\nafter swap:\n");
+                            //print_hashtable(table);
+                            pos = j+k;
+                            swapped = 1;
+                            break;
+                        }
+                    }
+                    bit_num--;
+                }
+                if (!swapped) { // if there are no swap candidates, rehash
+                    rehash_hashtable(&table, key, data);
+                    return table;
+                }
+            }
+            table->htbuckets[pos].rowid = data; // done
+            table->htbuckets[pos].key = key;
+            table->htbuckets[hash_value].bitmap[pos-hash_value] = 1;
+            break;
+        }
+    }
+    if (hashtable_full) { // if hash table is completely full, rehash
+        rehash_hashtable(&table, key, data);
+        return table; 
+    }
+
+    return table;
+}
+
+
+// double size of table and re-insert everything ("cause value is the value that caused the rehash. It will be inserted after")
+void rehash_hashtable(hashtable **ht, int cause_key, int cause_data) {
+    int original_size = (*ht)->tablesize;
+    printf("\nRehashing\n");
+
+    hashtable *ht2 = init_hashtable(original_size, (*ht)->nbsize); // "init_hashtable" initializes table with 2*original_size
+
+    for (int i=0; i<original_size; i++) // re-insert everything to new hash table
+        if ((*ht)->htbuckets[i].rowid != -1)
+            ht2 = insert_hashtable(ht2, (*ht)->htbuckets[i].key, (*ht)->htbuckets[i].rowid);
+    delete_hashtable(*ht);
+    *ht = ht2;
+
+    if (cause_key != -1) // try to insert the value that caused the rehash again
+        *ht = insert_hashtable(*ht, cause_key, cause_data);
+}
+
+
+void delete_hashtable(hashtable *ht) {
+    int i=0;
+    for(i=0; i< ht->tablesize; i++)
+       free(ht->htbuckets[i].bitmap);
     free(ht);
-    return;
 }
--- include/hashtable.h
+++ include/hashtable.h
@@ -3,29 +3,31 @@
 
 #include "relations.h"
 
+unsigned int hash2(unsigned int x, unsigned int max);
+
 typedef struct hashbucket hashbucket;
 typedef struct hashtable hashtable;
 
-
-struct hashbucket{
-    int rowid;
-    int *bitmap;
+struct hashbucket {
+    int rowid; // the data the bucket holds
+    int key; // the key that when hashed, leads to this bucket
+    int *bitmap; // bitmap showing which elements in the neighborhood originally hashed to this bucket
 };
 
-struct hashtable{
+struct hashtable {
     hashbucket *htbuckets;
-    //size of the table
-    int tablesize;
-    //size of the neighbourhood
-    int nbsize;
+    int tablesize; // size of the table
+    int nbsize; // size of the neighbourhood
 };
 
-// creates a bucket, n is the size of the bitmap
-hashbucket inithashbucket(int n);
-//creates an empty hastable, the size is going to be 2*n and H the size of neighbourhood
-hashtable *inithashtable(int n, int H);
+hashbucket init_hashbucket(int n); // creates a bucket, n is the size of the bitmap
+void bucket_swap(hashtable* table, int x, int y, int z);
 
+hashtable *init_hashtable(int n, int H); // creates an empty hastable, the size is going to be 2*n and H the size of neighbourhood
+hashtable* insert_hashtable(hashtable* table, int key, int data); // hashes key to insert data to hash table using hopscotch hashing
+void print_hashtable(hashtable* table);
+void delete_hashtable(hashtable *ht);
+void search_hashtable(hashtable* table, int key, int* ret);
 
-void deletehashtable(hashtable *ht);
-
-void expandhashtable(hashtable **ht);
+// doubles size of table, places everything again, and re-inserts value that caused rehash
+void rehash_hashtable(hashtable **ht, int cause_key, int cause_data);
