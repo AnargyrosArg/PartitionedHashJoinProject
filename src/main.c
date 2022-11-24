815b6c8417c6472ebc527c4522f9ef3bc9514a8e
Anargyros Argyros <argyrosan@gmail.com>
2022-11-24 16:22:31 +0200

    Revert "harness-compatible input handling"
    This reverts commit 43c0aa954cbfe2c9c528fca793fe98ca0fd78b94.

--- src/main.c
+++ src/main.c
@@ -1,69 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-#include "relations.h"
-#include "parser.h"
-
-#define MAX_LINE_SIZE 50
-#define MAX_N_TABLES 25
-#define MAX_N_QUERIES 50
+#include <time.h>
+#include "join.h"
 
 
+table* tables;
 
 
 int main(int argc, char** argv) {
-    //consider making these global for easy access
-    //array that keeps the input relations
-    table* tables;
-    size_t n_tables=0;
-    //array with info for input queries
-    QueryInfo* queries;
-    size_t n_queries=0;
-
-    //allocate table array
-    tables = malloc(MAX_N_TABLES * sizeof(table));
-    queries = malloc(MAX_N_QUERIES * sizeof(QueryInfo));
-
-    //buffer for reading input
-    char* line = malloc(MAX_LINE_SIZE*sizeof(char));
-    size_t line_max_size = MAX_LINE_SIZE;
-    int n_read;
-
-
-    //parse relation names and load them into memory
-    while((n_read=getline(&line,&line_max_size,stdin))>0){
-        //remove newline
-        line[strlen(line)-1] = 0;
-        if (strcmp(line,"Done")==0) break;
-        //store table
-        tables[n_tables++]=load_relation(line);
-    }
-
-    //parse batch of queries
-    while ((n_read=getline(&line,&line_max_size,stdin))>0) {
-        //remove newline
-        line[strlen(line)-1] = 0;
-        if (strcmp(line,"F")==0) continue;; // End of a batch
-        parse_query(line,&(queries[n_queries++]));
-    }
-
-    for(int i =0;i<n_queries;i++){
-        print_query_info(queries[i]);
-    }
- 
-    //free input buffer
-    free(line);
-    //free tables mem
-    for(int i =0;i<n_tables;i++){
-        delete_table(&(tables[i]));
-    }
-    free(tables);
     
-    //free queries mem
-    for(int i=0;i<n_queries;i++){
-        query_info_delete(&(queries[i]));
+    table ret = load_relation("workloads/small/r1");
+
+    for(uint64_t tuple=0;tuple<ret.num_tuples;tuple++){
+        for(uint64_t column=0;column<ret.num_colums;column++){
+            printf("%ld|",ret.table[column][tuple]);
+        }
+        printf("\n");
     }
-    free(queries);
-    return 0;
+
+    delete_table(&ret);
 }
